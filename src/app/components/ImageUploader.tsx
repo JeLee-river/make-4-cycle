bae6af59bf522fca0565f73d086b09c2d7e8aff4
Jeongeun Lee <leeju1009@gmail.com>
2023-09-29 01:04:20 +0900

    :sparkles: 사용자의 이미지를 업로드 하는 기능을 수행하는 컴포넌트 생성
    사용자의 이미지를 업로드 하는 기능을 수행하는 컴포넌트 생성

---
+++ src/app/components/ImageUploader.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useRef, useState } from 'react';
+import Image from 'next/image';
+import useTeachableModelPredict from '../hooks/useTeachableModelPredict';
+
+function ImageUploader() {
+  const [inputImageSource, setInputImageSource] = useState<string>('');
+
+  const handleChangePreviewImage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const usersInputImage = event.target.files;
+    if (usersInputImage) {
+      const usersInputImageURL = URL.createObjectURL(usersInputImage[0]);
+      setInputImageSource(usersInputImageURL);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type='file'
+          accept='image/png, image/jpeg, image/jpg'
+          onChange={handleChangePreviewImage}
+        />
+      </div>
+      {inputImageSource.length > 0 && (
+        <div className='ImageUploader_previewUploadImage'>
+          <Image
+            src={inputImageSource}
+            width={500}
+            height={500}
+            alt='업로드한 이미지'
+          />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ImageUploader;
