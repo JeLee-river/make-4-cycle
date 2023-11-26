d698e29d240942f8fd216245c8b592335ff0a1fa
Jeongeun Lee <leeju1009@gmail.com>
2023-11-26 23:45:10 +0900

    :sparkles: 임시 Home 페이지 렌더링
    임시 Home 페이지 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import ImageUploader from './components/ImageUploader';
 
 export default function Home() {
   return (
     <main>
-      <h1>Make A Cycle</h1>
+      <h1>Make 4 Cycle</h1>
+      <ImageUploader />
     </main>
   );
 }
