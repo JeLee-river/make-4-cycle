82e5c6fafcbda65b91dc2f070628e7faca862feb
jeongeunLee <leeju1009@gmail.com>
2023-12-27 15:20:05 +0900

    refactor: 변수명 오타 수정

--- src/app/hooks/useTeachableModelPredict.tsx
+++ src/app/hooks/useTeachableModelPredict.tsx
@@ -1,10 +1,10 @@
 import * as tmImage from '@teachablemachine/image';
 
-const teachableMarchineURL = process.env.NEXT_PUBLIC_TEACHABLE_MACHINE_URL;
+const teachableMachineURL = process.env.NEXT_PUBLIC_TEACHABLE_MACHINE_URL;
 
 async function useTeachableModelPredict(image: HTMLImageElement) {
-  const modelURL = `${teachableMarchineURL}model.json`;
-  const metadataURL = `${teachableMarchineURL}metadata.json`;
+  const modelURL = `${teachableMachineURL}model.json`;
+  const metadataURL = `${teachableMachineURL}metadata.json`;
   const model = await tmImage.load(modelURL, metadataURL);
   const predictions = await model.predictTopK(image, 10);
   const maximumPrediction = predictions.reduce((prevPredict, targetPredict) => {
