04ee5da419b4e106b5e1ccd773b0353fc0eaa82a
jeongeunLee <leeju1009@gmail.com>
2024-01-14 23:03:05 +0900

    :sparkles: Home 화면에서 dashboard 페이지로 이동하도록 기능 추가
    home 화면에서 dashboard 페이지로 이동하도록 코드를 수정함

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image';
 import styles from './page.module.css';
-import ImageUploader from './components/ImageUploader';
-import Dashboard from './dashboard/page';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <h1>Make 4 Cycle</h1>
-      <Dashboard />
+      <p>4단계의 분리수거로 순환하는 지구</p>
+      <Link href='/dashboard'>시작하기</Link>
     </main>
   );
 }
