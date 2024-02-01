6ce47724c70ef36b6c9c7715d248f79750d491e2
jeongeunLee <leeju1009@gmail.com>
2024-02-02 01:15:07 +0900

    :card_file_box: Subcategory를 고려하여 db에서 정보를 찾는 로직 수정
    subcategory로 보다 세분화된 분리수거 정보를 찾도록 로직 수정

--- src/lib/db/recycleInfoDAO.ts
+++ src/lib/db/recycleInfoDAO.ts
@@ -2,9 +2,18 @@ import { RecycleInformationType } from '@/app/types/types';
 import { RecycleInfo } from '@database/model';
 
 const recycleInfoDAO = {
-  findRecycleInfo: async (className: string) => {
-    const recycleInfo = await RecycleInfo.findOne({ className });
-    return recycleInfo;
+  findRecycleInfo: async (category: string, subCategory?: string) => {
+    const categorizedRecycleInfo = await RecycleInfo.find({ category });
+
+    if (subCategory) {
+      const targetRecycleInfo = categorizedRecycleInfo.find(
+        (information) => information.subCategory === subCategory
+      );
+
+      return targetRecycleInfo;
+    }
+
+    return categorizedRecycleInfo;
   },
 
   addRecycleInfo: async (recycleResource: RecycleInformationType) => {
