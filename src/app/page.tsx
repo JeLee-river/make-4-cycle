e78dd06b68f96d9cce7f56db34fa892276cb476d
jeongeunLee <leeju1009@gmail.com>
2024-08-24 23:30:12 +0900

    :lipstick: 메인페이지 스타일 초안 설정
    메인페이지 스타일 초안 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,14 @@ export default function Home() {
     <main>
       <section className='flex items-center justify-center bg-cover bg-center bg-blend-darken bg-black bg-opacity-30 bg-[url("https://res.cloudinary.com/dr26wooar/image/upload/v1711363534/make4cycle/main.jpg")] min-w-full min-h-dvh'>
         <div className='flex flex-col items-center justify-center text-center w-full h-full'>
-          <h1 className='text-white text-7xl'>Make 4 Cycle</h1>
-          <p className='text-white text-2xl'>
+          <h1 className='m-2 text-white text-7xl font-medium'>Make 4 Cycle</h1>
+          <p className='m-1 text-white text-1xl'>
             4단계의 분리수거로 순환하는 지구
           </p>
-          <Link href='/image-uploader' className='text-white'>
+          <Link
+            href='/image-uploader'
+            className='m-5 p-2 min-w-24 rounded-3xl bg-yellow hover:bg-green text-dark-green hover:text-white text-1xl font-medium text-center'
+          >
             시작하기
           </Link>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,26 @@
 import type { Config } from 'tailwindcss';
+import { colors as defaultColors } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    fontWeight: {
+      regular: '400',
+      medium: '500',
+      bold: '700',
+    },
     extend: {
       backgroundImage: {},
       fontFamily: {
         sans: ['Goorm Sans', 'sans-serif'],
+      },
+      colors: {
+        'dark-green': '#1c3732',
+        'deep-green': '#2c5d4a',
+        olive: '#4a855b',
+        green: '#78ad65',
+        chartreuse: '#b2d46b',
+        yellow: '#f9f871',
       },
     },
   },
