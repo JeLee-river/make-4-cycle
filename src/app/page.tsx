e987b344218e71f79400f695341ce81e291351a8
jeongeunLee <leeju1009@gmail.com>
2024-08-24 13:28:35 +0900

    :lipstick: 메인페이지 타이틀이 화면 중앙에 오도록 수정
    메인페이지 타이틀이 화면 중앙에 오도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <section className='bg-cover bg-center bg-blend-darken bg-black bg-opacity-30 bg-[url("https://res.cloudinary.com/dr26wooar/image/upload/v1711363534/make4cycle/main.jpg")] min-w-full min-h-screen'>
-        <h1 className='text-white'>Make 4 Cycle</h1>
-        <p className='text-white'>4단계의 분리수거로 순환하는 지구</p>
-        <Link href='/image-uploader' className='text-white'>
-          시작하기
-        </Link>
+      <section className='flex items-center justify-center bg-cover bg-center bg-blend-darken bg-black bg-opacity-30 bg-[url("https://res.cloudinary.com/dr26wooar/image/upload/v1711363534/make4cycle/main.jpg")] min-w-full min-h-dvh'>
+        <div className='flex flex-col items-center justify-center text-center w-full h-full'>
+          <h1 className='text-white text-7xl'>Make 4 Cycle</h1>
+          <p className='text-white text-2xl'>
+            4단계의 분리수거로 순환하는 지구
+          </p>
+          <Link href='/image-uploader' className='text-white'>
+            시작하기
+          </Link>
+        </div>
       </section>
     </main>
   );
