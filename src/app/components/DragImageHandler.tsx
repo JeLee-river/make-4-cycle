182006a0243cbc8656ae861ac44bb4cbc3acaf12
jeongeunLee <leeju1009@gmail.com>
2024-08-27 22:01:09 +0900

    :recycle: 드롭박스 내부 설명문구 수정
    드롭박스 내부 설명문구 수정

--- src/app/components/DragImageHandler.tsx
+++ src/app/components/DragImageHandler.tsx
@@ -4,7 +4,7 @@ function DragImageHandler({
 }: {
   setInputImage: React.Dispatch<React.SetStateAction<string>>;
   handleUploadClick: (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => void;
 }) {
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
@@ -19,7 +19,7 @@ function DragImageHandler({
       const validType = ['image/png', 'image/jpeg', 'image/jpg'];
       if (!userImage || !validType.includes(userImage.type)) {
         throw new Error(
-          '지원하지 않는 형식입니다. 이미지 파일을 업로드 해주세요.'
+          '지원하지 않는 형식입니다. 이미지 파일을 업로드 해주세요.',
         );
       }
 
@@ -36,9 +36,14 @@ function DragImageHandler({
         onDragOver={handleDragOver}
         onDrop={handleDrop}
         onClick={handleUploadClick}
-        className='flex h-64 w-96 cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 transition-colors hover:border-gray-400'
+        className='flex h-64 w-96 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border-2 border-dashed border-gray-300 bg-slate-300 transition-colors hover:border-gray-400'
       >
-        드래그 앤 드롭
+        <h3 className='text-2xl font-medium text-olive'>
+          이미지 드래그 앤 드롭 또는 클릭
+        </h3>
+        <p className='text-1xl font-regular text-olive'>
+          이미지 파일 형식만 가능합니다. (jpg, png, jpeg)
+        </p>
       </div>
     </>
   );
