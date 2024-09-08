45ba2f9e1a2da947b55f0ce74ca3c69dafc829fc
jeongeunLee <leeju1009@gmail.com>
2024-09-08 13:48:48 +0900

    :lipstick: 드롭박스 이미지 url 변경 및 클래스 순서 변경
    드롭박스 이미지 url 변경 및 클래스 순서 변경

--- src/app/components/DragImageHandler.tsx
+++ src/app/components/DragImageHandler.tsx
@@ -36,7 +36,7 @@ function DragImageHandler({
         onDragOver={handleDragOver}
         onDrop={handleDrop}
         onClick={handleUploadClick}
-        className='bg-ivory flex h-96 min-h-min w-[32rem] min-w-min cursor-pointer flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-gray-300 bg-dropbox bg-cover bg-center bg-no-repeat p-3 transition-colors hover:border-gray-400'
+        className='flex h-96 min-h-min w-[32rem] min-w-min cursor-pointer flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-gray-300 bg-ivory bg-dropbox bg-cover bg-center bg-no-repeat p-3 transition-colors hover:border-gray-400'
       >
         <h3 className='text-2xl font-medium leading-4 text-dark-green'>
           이미지 드래그 앤 드롭
