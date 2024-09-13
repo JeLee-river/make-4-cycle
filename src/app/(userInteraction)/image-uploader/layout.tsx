c2ab6010f970a29aa949f42bd8f11880bd4d524f
jeongeunLee <leeju1009@gmail.com>
2024-09-14 00:58:49 +0900

    :sparkles: 메인페이지로 이동하는 로고 추가
    메인페이지로 이동하는 로고 추가

--- src/app/(userInteraction)/image-uploader/layout.tsx
+++ src/app/(userInteraction)/image-uploader/layout.tsx
@@ -1,10 +1,19 @@
+import Link from 'next/link';
+
 export default function ImageUploaderLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className='flex h-svh min-h-min w-full min-w-min items-center justify-center bg-dark-green p-6'>
+    <section className='flex h-svh min-h-min w-full min-w-min flex-col items-center justify-center bg-dark-green p-6'>
+      <Link
+        title='Make 4 cycle 메인페이지로 이동'
+        href='/'
+        className='m-4 text-4xl text-white'
+      >
+        Make 4 Cycle
+      </Link>
       {children}
     </section>
   );
