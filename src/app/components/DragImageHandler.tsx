7b606ea2fb14245bd4482dc39e8f13394ce35346
jeongeunLee <leeju1009@gmail.com>
2024-09-15 00:42:19 +0900

    :lipstick: 드롭박스의 hover 테두리를 border에서 outline으로 변경
    border가 생성될 때 다른 컨텐츠의 레이아웃에 영향을 주는 것을 고려하여 border를 outline으로 수정

--- src/app/components/DragImageHandler.tsx
+++ src/app/components/DragImageHandler.tsx
@@ -38,7 +38,7 @@ function DragImageHandler({
         onDragOver={handleDragOver}
         onDrop={handleDrop}
         onClick={handleUploadClick}
-        className='box-content flex h-96 min-h-min w-[32rem] min-w-min cursor-pointer flex-col items-center justify-center gap-4 rounded bg-ivory bg-dropbox bg-cover bg-center bg-no-repeat p-3 hover:border-4 hover:border-dashed hover:border-dark-green'
+        className='mt-5 flex aspect-[8/5] min-h-min w-1/3 min-w-min cursor-pointer flex-col items-center justify-center gap-4 rounded bg-ivory bg-dropbox bg-cover bg-center bg-no-repeat p-3 hover:outline-dotted hover:outline-2 hover:outline-white'
       >
         <Image
           src={process.env.NEXT_PUBLIC_UPLOAD_ICON}
@@ -46,13 +46,13 @@ function DragImageHandler({
           height={100}
           alt='업로드 일러스트'
         />
-        <h3 className='text-2xl font-medium leading-3 text-dark-green'>
+        <h3 className='text-1xl font-medium leading-normal text-dark-green'>
           드래그 앤 드롭
         </h3>
-        <span className='text-1xl relative font-regular text-dark-green before:absolute before:right-8 before:top-1/2 before:inline-block before:h-px before:w-full before:-translate-y-1/2 before:bg-dark-green before:content-[""] after:absolute after:left-8 after:top-1/2 after:inline-block after:h-px after:w-full after:-translate-y-1/2 after:bg-dark-green after:content-[""]'>
+        <span className='relative text-xs font-regular text-dark-green before:absolute before:right-8 before:top-1/2 before:inline-block before:h-px before:w-full before:-translate-y-1/2 before:bg-dark-green before:content-[""] after:absolute after:left-8 after:top-1/2 after:inline-block after:h-px after:w-full after:-translate-y-1/2 after:bg-dark-green after:content-[""]'>
           또는
         </span>
-        <p className='text-2xl font-medium leading-3 text-dark-green'>
+        <p className='text-1xl font-medium leading-normal text-dark-green'>
           클릭하여 파일 선택
         </p>
       </div>
