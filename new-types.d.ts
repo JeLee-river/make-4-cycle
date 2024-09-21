9934d205aa618d367a563eb77b89324e86fe4154
jeongeunLee <leeju1009@gmail.com>
2024-09-21 12:39:34 +0900

    :recycle: 분리수거 정보 페이지의 이미지를 background로 수정
    분리수거 정보 페이지의 이미지를 background로 수정

--- new-types.d.ts
+++ new-types.d.ts
@@ -1,5 +1,13 @@
+import 'react';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_UPLOAD_ICON: string;
+  }
+}
+
+declare module 'react' {
+  interface CSSProperties {
+    '--image-url'?: string;
   }
 }
